d6916820aa037eeee5dc4ea9c57c2a58591d74e6
cocoatomo <cocoatomo77@gmail.com>
2011-05-21 07:12:15 +0900

    add Django version

--- deferred.py
+++ deferred.py
@@ -20,6 +20,11 @@
 See deferred.py for full documentation.
 """
 
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from google.appengine.dist import use_library
+use_library('django', '0.96')
 
 
 
