39ee68b52a476230e32d63c50fc6acc29ae69fc1
cocoatomo <cocoatomo77@gmail.com>
2011-10-18 01:40:06 +0900

    remove use_library

--- static.py
+++ static.py
@@ -1,8 +1,8 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from google.appengine.dist import use_library
-use_library('django', '0.96')
+# from google.appengine.dist import use_library
+# use_library('django', '0.96')
 
 import datetime
 import hashlib
